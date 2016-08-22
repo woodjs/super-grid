226a6f0f4776cff315dfad6c614c1764e0a582b7
yinyanlv <yanlv.yin@gmail.com>
2016-08-22 17:45:34 +0800

    提交修改

--- js/grid.js
+++ js/grid.js
@@ -11,9 +11,50 @@
 })(function () {
   "use strict";
 
+  var grid = {
+    init: function () {
+
+    },
+
+    initEvent: function () {
+
+    }
+
+  };
+
+  var util = {
+    getScrollbarWidth: function () {
+      var a = document.createElement('div');
+      var b = document.createElement('div');
+
+      a.style.overflowY = 'hidden';
+      b.style.height = '1px';
+      a.appendChild(b);
+      document.body.appendChild(a);
+      var tempWidth = b.clientWidth;
+      a.style.overflowY = 'scroll';
+
+      return tempWidth - b.clientWidth;
+    }
+  };
+
   function Grid() {
-    alert(1);
+
   }
+
+
+  $.fn.grid = function () {
+
+    return '';
+  };
+
+  $.fn.grid.methods = function () {
+
+  };
+
+  $.fn.grid.defaults = function () {
+
+  };
 
   return Grid;
 });
