0ee95c83006ac448cdb42e262d4016f9fb2dc82e
yinyanlv <yanlv.yin@gmail.com>
2016-09-19 16:23:39 +0800

    提交修改

--- js/query.js
+++ js/query.js
@@ -17,10 +17,14 @@
 
     init: function (target) {
       var self = this;
+      var opts = $(target).data('query').options;
 
+      opts.onBeforeRender && opts.onBeforeRender.call(null);
       self.initGlobalScope(target);
       self.initJqueryObject(target);
       self.initEvent(target);
+      self.loadData(target);
+      opts.onAfterRender && opts.onAfterRender.call(null);
     },
 
     initGlobalScope: function (target) {
@@ -49,61 +53,74 @@
 
     initEvent: function (target) {
       var self = this;
+      var $target = $(target);
+      var opts = $target.data('query').options;
 
-      target.jq.$querySelectList.on({
-        click: function (e) {
-          var $this = $(this);
+      if (opts.selectMode === 'native') {
+        target.jq.$querySelectList.on({
+          click: function () {
+            var $this = $(this);
 
-          if ($this.data('loaded') != 'true') {
-            self.loadSelectData(target, $this);
+            if ($this.data('noCache') === true || $this.data('loaded') !== true) {
+
+              self.loadSelectData(target, $this);
+            }
+          },
+          change: function () {
+            var $this = $(this);
+            var $nextSelect = $target.find($this.data('next'));
+
+            if ($nextSelect.length) self.loadSelectData(target, $nextSelect, true);
           }
-        },
-        change: function () {
-          var $this = $(this);
-
-          self.clearSelect(target, $this);
-        }
-      });
+        });
+      }
 
       target.jq.$btnQuery.on({
         click: function () {
           var params = self.getParams(target);
 
+          opts.onBeforeQuery && opts.onBeforeQuery.call(null);
           self.doQuery(target, params);
+          opts.onAfterQuery && opts.onAfterQuery.call(null);
         }
       });
 
       target.jq.$btnReset.on({
         click: function () {
+
+          opts.onBeforeReset && opts.onBeforeReset.call(null);
           for (var i = 0; i < target.jq.$queryAllInputList.length; i++) {
             self.resetItem($(target.jq.$queryAllInputList[i]));
           }
+          opts.onAfterReset && opts.onAfterReset.call(null);
         }
       });
     },
 
-    loadSelectData: function (target, $select) {
+    loadData: function (target) {
+      var self = this;
+      var $selectList = target.jq.$querySelectList;
+      var len = $selectList.length;
+      var $temp;
+
+      for (var i = 0; i < len; i++) {
+        $temp = $($selectList[i]);
+        if ($temp.data('preload') === true) {
+          self.loadSelectData(target, $temp);
+        }
+      }
+    },
+
+    loadSelectData: function (target, $select, isClearSelf) {
       var self = this;
       var url = $select.data('url');
+      var opts = $(target).data('query').options;
       var queryString = self.getQueryString(target, $select);
 
-      var data = [{
-        id: 1,
-        code: 'temp1',
-        name: '哈哈' + Math.random()
-      }, {
-        id: 2,
-        code: 'temp2',
-        name: '呵呵' + Math.random()
-      }, {
-        id: 3,
-        code: 'temp3',
-        name: '嘻嘻' + Math.random()
-      }, {
-        id: 4,
-        code: 'temp4',
-        name: '嘿嘿' + Math.random()
-      }];
+      if (queryString === false) {
+        self.clearSelect(target, $select, isClearSelf);
+        return;
+      }
 
       $.ajax({
         url: url + queryString,
@@ -112,14 +129,15 @@
         timeout: 3000,
         success: function () {
           self.clearSelect(target, $select);
-          self.updateSelect($select, data);
-          $select.data('loaded', 'true');
+          self.updateSelect(target, $select, data);
+          $select.data('loaded', true);
         },
         error: function () {
           self.clearSelect(target, $select);
-          self.updateSelect($select, data);
-          $select.data('loaded', 'true');
-        }
+          self.updateSelect(target, $select, data);
+          $select.data('loaded', true);
+        },
+        // error: opts.onSelectAjaxError && opts.onSelectAjaxError.call(null, url + queryString)
       });
     },
 
@@ -134,44 +152,53 @@
 
       for (var i = 0; i < len; i++) {
         $temp = $target.find(dependenciesIdList[i]);
+        if ($temp.val() === '') return false;
         list.push($temp[0].name + '=' + $temp.val());
       }
 
       return len ? '?' + list.join('&') : '';
     },
 
-    clearSelect: function (target, $select) {
+    clearSelect: function (target, $select, isClearSelf) {
       var self = this;
+      var opts = $(target).data('query').options;
+      var withAll = $select.data('withall') || opts.withAll;
+      var withAllText = $select.data('withalltext') || opts.withAllText;
       var clearIdStr = $select.data('clearids');
       var clearIdList = clearIdStr && JSON.parse(clearIdStr.replace(/\'/g, '"'));
       var len = clearIdList && clearIdList.length;
       var $target = $(target);
       var $temp;
 
+      if (isClearSelf)  $select.html(withAll ? '<option value="">' + withAllText + '</option>' : '');
+
       for (var i = 0; i < len; i++) {
         $temp = $target.find(clearIdList[i]);
-        $temp.val('');
-        $temp.html('<option value="">全部</option>');
-        $temp.data('loaded', 'false');
+        $temp.html(withAll ? '<option value="">' + withAllText + '</option>' : '');
+        $temp.data('loaded', false);
       }
     },
 
-    updateSelect: function ($select, data) {
+    updateSelect: function (target, $select, data) {
       var self = this;
-      var html = '<option value="">全部</option>';
+      var opts = $(target).data('query').options;
+      var withAll = $select.data('withall') || opts.withAll;
+      var withAllText = $select.data('withalltext') || opts.withAllText;
+      var html = withAll ? '<option value="">' + withAllText + '</option>' : '';
 
       for (var i = 0; i < data.length; i++) {
-        html += '<option value="'+ data[i].code +'">'+ data[i].name +'</option>';
+        html += '<option value="' + data[i].code + '">' + data[i].name + '</option>';
       }
 
       $select.html(html);
+      $select.val($select.data('value'));
     },
 
     resetItem: function ($item) {
       var self = this;
 
       if ($item.is('select')) {
-        $item.data('loaded', 'false');
+        $item.data('loaded', false);
       }
 
       $item.val('');
@@ -180,6 +207,7 @@
     getParams: function (target) {
       var self = this;
       var len = target.jq.$queryItemList.length;
+      var opts = $(target).data('query').options;
       var $temp;
       var $input;
       var value;
@@ -190,7 +218,7 @@
         $input = $temp.find('.' + target.ns.cssPrefix + 'query-input');
         value = $input.val();
         if ($temp.is('.' + target.ns.cssPrefix + 'query-required') && value == '') {
-          alert('有必填项未填！');
+          opts.onRequiredIsEmpty.call(null);
           return;
         }
         if (value != '') {
@@ -200,6 +228,7 @@
 
       return result;
     },
+
     doQuery: function (target, params) {
       var self = this;
       var opts = $(target).data('query').options;
@@ -207,20 +236,18 @@
       target.ns.params = params;
 
       $.ajax({
-        url: '/',
-        type: 'POST',
-        cache: false,
-        timeout: 3000,
+        url: opts.url,
+        type: opts.method,
+        cache: opts.cache,
+        timeout: opts.timeout,
         data: target.ns.params,
         dataType: 'json',
         beforeSend: opts.onAjaxBeforeSend,
         complete: opts.onAjaxComplete,
         error: function () {
-
           opts.onAjaxError && opts.onAjaxError.apply(null, Array.prototype.slice.apply(null, arguments));
         },
         success: function (result) {
-
           opts.onAjaxSuccess && opts.onAjaxSuccess.apply(null, [result]);
         }
       });
@@ -248,8 +275,11 @@
 
   $.fn.query.defaults = {
     cssPrefix: 's-',
+    selectMode: 'native',
+    withAll: true,
+    withAllText: '全部',
     url: '',
-    method: 'GET',
+    method: 'POST',
     cache: false,
     timeout: 3000,
     params: null,
@@ -262,6 +292,8 @@
     onBeforeQuery: null,
     onAfterQuery: null,
     onBeforeReset: null,
-    onAfterReset: null
+    onAfterReset: null,
+    onRequiredIsEmpty: function () {},
+    onSelectAjaxError: function () {}
   };
 });
